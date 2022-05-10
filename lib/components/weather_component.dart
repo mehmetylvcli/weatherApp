93ab53c5854ab2bc72d1c99a7b609bf0dab95845
metassup <yalvacli82@gmail.com>
2022-05-10 13:30:26 +0300

    code clean

--- lib/components/weather_component.dart
+++ lib/components/weather_component.dart
@@ -29,7 +29,6 @@ class _WeatherComponentState extends State<WeatherComponent> {
 
     var weatherData = WeatherModel.fromJson(res);
 
-    // print(weatherData.list![9].weather![0].description);
 
     for (var item in weatherData.list!) {
       if (dailyWeather.isNotEmpty) {
@@ -70,56 +69,15 @@ class _WeatherComponentState extends State<WeatherComponent> {
 
   final Geolocator geolocator = Geolocator();
 
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-
-    // _getCurrentLocation();
-
-    return await Geolocator.getCurrentPosition();
-  }
+ 
 
   void getLocation() async {
     bool serviceEnabled;
     LocationPermission permission;
 
-    // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
+
       return Future.error('Location services are disabled.');
     }
 
@@ -127,17 +85,12 @@ class _WeatherComponentState extends State<WeatherComponent> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
+
         return Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
@@ -150,21 +103,6 @@ class _WeatherComponentState extends State<WeatherComponent> {
     _getAddressFromLatLng(position.latitude, position.longitude);
   }
 
-  // _getCurrentLocation() {
-  //   Geolocator.getCurrentPosition(
-  //           desiredAccuracy: LocationAccuracy.best,
-  //           forceAndroidLocationManager: true)
-  //       .then((Position position) {
-  //     setState(() {
-  //       _currentPosition = position;
-  //       _getAddressFromLatLng();
-  //       getWeatherProperty(
-  //           _currentPosition!.latitude, _currentPosition!.longitude);
-  //     });
-  //   }).catchError((e) {
-  //     print(e);
-  //   });
-  // }
 
   _getAddressFromLatLng(lat, long) async {
     try {
@@ -190,7 +128,6 @@ class _WeatherComponentState extends State<WeatherComponent> {
   void initState() {
     super.initState();
     getLocation();
-    // _determinePosition();
   }
 
   @override
